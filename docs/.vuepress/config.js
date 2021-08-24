a9222ae3cbfe0b897d6872bd1fc82f71ef97ed7e
yangzheli <1936558266@qq.com>
2021-08-24 08:47:26 +0800

    adjust directory structure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,7 @@
 const path = require('path')
-var gitalkConfig = require('./config/gitalk.js')
-var navConfig = require('./config/nav.js')
+
 module.exports = {
-    // theme: path.resolve(__dirname, '../../ting/'),
-    title: 'YATING', //网站名称
+    title: 'yangzheli', //网站名称
     description: '用心写代码，不辜负程序员之名', //网站描述
     //head标签
     head: [
@@ -11,21 +9,23 @@ module.exports = {
         ['script', { src: 'https://cdn.bootcss.com/jquery/3.4.1/jquery.js' }]
     ],
     themeConfig: {
-        author: '雅婷',
+        author: 'yangzheli',
         headImg: '/img/yating.jpg', //头像
         //导航栏
-        nav: navConfig,
+        nav: [
+            { text: '鱼', link: '/' },
+            { text: '目次', link: '/catalog' },
+            { text: 'Github', type: 'url', link: 'https://github.com/Chenyating' },
+        ],
         catalogUrl: '/catalog', //必填
         lastUpdated: 'Last Updated',
         smoothScroll: true,
         pageNum: 10, //目录每页显示条数
         // live2dModel: '/live2d/model/poi/poi.model.json', //live2d模型路径
         //gitalk留言设置
-        gitalk: gitalkConfig,
         footer: '粤ICP备案号：18150247号'
     },
     markdown: {
         lineNumbers: true
     },
-    theme: 'theme'
 }
